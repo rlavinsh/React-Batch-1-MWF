007cf3b5d0578caa95ef0267d91d3fbc419e5003
rlavinsh <rlavinsh@gmail.com>
2026-09-21 18:14:57 +0530

    code updated

--- Day-5-Part-2/src/App.jsx
+++ Day-5-Part-2/src/App.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Card from "./components/Card";
+import userData from "../product.json";
 const App = () => {
+  // Parent -> child
   const arr = [10, 20, 30];
+  const user = {
+    firstName: "Lavinsh",
+    age: 25,
+  };
+  // console.log(userData[0].id);
+
   return (
-    <div>
-      <Card name="Ankit" city="Bhopal" age={25} number={arr} />
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+      {userData.map((product, index) => {
+        return (
+          <Card
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            category={product.category}
+            brand={product.brand}
+          />
+        );
+      })}
+      {/* <Card name="Ankit" city="Bhopal" age={25} number={arr} data={user} />
       <Card name="Rahul" city="Indore" age={26} number={arr} />
       <Card name="Rohan" city="Gwalior" age={27} number={arr} />
+      <Card name="Ritik" /> */}
     </div>
   );
 };
--- Day-5-Part-2/src/components/Card.jsx
+++ Day-5-Part-2/src/components/Card.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-const Card = (props) => {
-  console.log(props);
-
+const Card = ({
+  name = "user",
+  price = "Not provided",
+  category = 0,
+  brand = "Not Provided",
+}) => {
   return (
     <div className="card">
-      <h1>Hello {props.name}</h1>
-      <h2>City: {props.city}</h2>
-      <h3>Age: {props.age}</h3>
-
-      {/* <h1>Hello {name}</h1>
-      <h3>City: {city}</h3> */}
+      <h1>{name}</h1>
+      <h3>Price: {price}</h3>
+      <h4>category: {category}</h4>
+      <h4>brand: {brand}</h4>
     </div>
   );
 };
